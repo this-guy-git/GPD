c8ee4b73f62bf29c8244d484a3f88cafd5695f0a
this guy <128647311+this-guy-git@users.noreply.github.com>
2025-06-20 13:56:28 -0700

    Update time.py

--- packages/time/time.py
+++ packages/time/time.py
@@ -1,1 +1,11 @@
+import time
+import datetime
 
+def current_time():
+    return time.time()
+
+def formatted_time():
+    return time.strftime("%H:%M:%S")
+
+def future_date(days):
+    return (datetime.datetime.now() + datetime.timedelta(days=days)).strftime("%Y-%m-%d")
